236500c29687f055a97dd8cf504af774b2c9e7e2
ulrichozanne <51369124+ulrichozanne@users.noreply.github.com>
2020-01-13 11:53:08 +0100

    navbar

--- interface.php
+++ interface.php
@@ -3,46 +3,31 @@
 <head>
 	<title>Interfaces de choix</title>
 	<link rel="stylesheet" type="text/css" href="css\style.css">
-
-
-<style>
-@import url('https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap');
-</style>
-
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<style>
+		@import url('https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap');
+	</style>
 </head>
 <body class="bg">
-
 <header>
 		    <nav>
-		       <p>Bonjour Rémi Leroy, que voulez vous faire ? </p>
+		       <p class="container">Bonjour Rémi Leroy, que voulez vous faire ? </p>
 		   </nav>
 </header>
 
-
-<section class="sectionmenu">
-	<ul>
-		<a><li>Evaluation</li></a>
-		<a><li>Créer un stagiaire</li></a>
-		<a><li>Stagiaire</li></a>
-	</ul>
-</section>
-
-
-
-
-
-
-
-
-
+<div class="topnav">
+  <a class="active" href="#home">Accueil</a>
+  <a href="#news">Evaluation</a>
+  <a href="#about">Rechercher une évaluation</a>
+	<a href="https://calendar.google.com/calendar/r?tab=wc1" target="_blank">Consulter l'agenda</a>
+  <a href="#about">Créer un stagiaire</a>
+	<a href="#about">Créer un compte utilisateur</a>
+</div>
 
 <footer>
-	
 <a href="index.php" class="afooter">Se déconnecter ?</a>
 <a href="tableau.php" class="testa">Tableau</a>
-
 </footer>
-
 
 </body>
 </html>
