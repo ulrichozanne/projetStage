390518c0500301fc20d4bdcb21cd7b366397fa0f
ulrichozanne <51369124+ulrichozanne@users.noreply.github.com>
2020-01-13 15:28:32 +0100

    responsive page accueil

--- interface.php
+++ interface.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Interfaces de choix</title>
+	<title>Accueil</title>
 	<link rel="stylesheet" type="text/css" href="css\style.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<style>
 		@import url('https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap');
 	</style>
 </head>
-<body class="bg">
-<header>
+<body class="bg" id="global">
+<div class="background">
 		    <nav>
-		       <p class="container">Bonjour Rémi Leroy, que voulez vous faire ? </p>
+		       <p class="container">Bonjour [Rémi Leroy], que voulez-vous faire ? </p>
 		   </nav>
-</header>
+</div>
 
-<div class="topnav">
-  <a class="active" href="#home">Accueil</a>
+<div class="container">
+<div class="topnav" id="global">
+  <a class="active" href="#">Accueil</a>
   <a href="#news">Evaluation</a>
   <a href="#about">Rechercher une évaluation</a>
 	<a href="https://calendar.google.com/calendar/r?tab=wc1" target="_blank">Consulter l'agenda</a>
   <a href="#about">Créer un stagiaire</a>
 	<a href="#about">Créer un compte utilisateur</a>
 </div>
+</div>
 
+<div class="container">
 <footer>
-<a href="index.php" class="afooter">Se déconnecter ?</a>
+<a href="index.php" class="afooter">Se déconnecter</a>
 <a href="tableau.php" class="testa">Tableau</a>
 </footer>
+</div>
 
 </body>
 </html>
